fd8357a7b32815abe187638986f4e99c435c1c05
aPannell064 <adamkpannell@gmail.com>
2025-02-05 10:18:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,28 +1,25 @@
 function mergesort(array) {
     var hi, mid;
-    for (var len = 2; len < array.length*2; len*=2)
-    {
-        hi = (len > array.length ? array.length : len)-1; 
-        for (var lo = 0; lo < array.length; lo+=len)
-        {   
+    //Resize the length of arrays being merged
+    for (var len = 2; len < array.length*2; len*=2) {
+        hi = (len > array.length ? array.length : len)-1;
+        //Iterate through the array merging each section of length: len
+        for (var lo = 0; lo < array.length; lo+=len) {
             mid = lo+ Math.floor((len-1)/2);
-            merge(array, lo, mid, hi);
-            hi = (hi + len > array.length-1) ? array.length-1 : hi + len;   
+            array = merge(array, lo, mid, hi);
+            hi = (hi + len > array.length-1) ? array.length-1 : hi + len;
         }
-        
     }
     return array;
 }
 
-function merge(x, lo, mid, hi)
-{
-    var tmp;
-    var a = lo, b = mid+1
+//Merges values past the midpoint by shifting elements in front of it
+function merge(x, lo, mid, hi) {
+    var tmp, a = lo, b = mid+1;
     for(var k = lo; k<= hi; k++) {
         if(a >= b || (b <= hi && x[a] > x[b])) {
             tmp = x[b];
-            for(var i = b; i > a; i--)
-            {
+            for(var i = b; i > a; i--) {
                 x[i] = x[i-1];
             }
             x[a] = tmp;
@@ -30,4 +27,5 @@ function merge(x, lo, mid, hi)
         }
         a++;
     }
+    return array;
 }
